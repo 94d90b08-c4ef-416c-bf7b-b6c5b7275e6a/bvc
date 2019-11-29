227e3cebe1f56281c6a1f10a4fc872bc9df4e90f
decay <dmitrysk@ro.ru>
2019-11-29 23:28:38 +0300

    Draft for new repository creating

---
+++ include/process.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <memory>
+#include <cstdio>
+#include <array>
+
+std::string exec(std::string command){
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.c_str(), "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
